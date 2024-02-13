189fb8eee0f65a42cb4fea63399031ea4b3fb15c
Anastasia-Artemova <anastasia.r.artemova@gmail.com>
2024-02-13 18:36:12 +0200

    Refactor: simplify time calculation

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -2,6 +2,7 @@ import { useInfiniteQuery } from "@tanstack/react-query";
 import { GameQuery } from "../App";
 import APIClient, { FetchResponse } from "../services/api-client";
 import { Platform } from "./usePlatforms";
+import ms from "ms";
 
 export interface Game {
     id: number;
@@ -30,7 +31,7 @@ const useGames = (gameQury: GameQuery) => useInfiniteQuery<FetchResponse<Game>, 
         return lastPage.next ? allPages.length + 1 : undefined;
     },
     initialPageParam: 1,
-    staleTime: 24 * 60 * 60 * 1000,
+    staleTime: ms('24h'),
 });
 
 
