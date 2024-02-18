d0f6b880d3b8f011c79ae932c17d32d44b80fe6f
Anastasia-Artemova <anastasia.r.artemova@gmail.com>
2024-02-18 18:51:27 +0100

    Refactor: move entity interface

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,20 +1,9 @@
 import { useInfiniteQuery } from "@tanstack/react-query";
 import useGameQueryStore from "../store";
 import APIClient, { FetchResponse } from "../services/api-client";
-import { Platform } from "./usePlatforms";
 import ms from "ms";
+import { Game } from "../entities/Game";
 
-export interface Game {
-    id: number;
-    name: string;
-    slug: string;
-    background_image: string;
-    parent_platforms: {platform: Platform}[];
-    metacritic: number;
-    rating_top: number;
-    description_raw: string;
-  }
-  
   const apiClient = new APIClient<Game>('/games');
 
 const useGames = () => {
